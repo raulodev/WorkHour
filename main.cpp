bf08f610d783f6d3ce32de48d50dbfac022b969a
raulodev <raulodev@gmail.com>
2023-12-10 23:02:02 -0500

    update design

--- main.cpp
+++ main.cpp
@@ -14,8 +14,14 @@ std::string getTime(int hours, int minutes, int seconds)
 {
 
 	std::ostringstream oss;
-	oss << std::setfill('0') << std::setw(2) << hours << ":" << std::setw(2) << minutes << ":" << std::setw(2) << seconds;
+
+	oss << std::setfill('0')
+		<< std::setw(2) << hours << " : "
+		<< std::setw(2) << minutes << " : "
+		<< std::setw(2) << seconds;
+
 	std::string time = oss.str();
+
 	return time;
 }
 
@@ -30,43 +36,97 @@ void updateTime(int &hours, int &minutes, int &seconds, bool &start)
 		{
 			minutes += 1;
 			seconds = 0;
-		};
+		}
 
 		if (minutes == 60)
 		{
 			hours += 1;
 			minutes = 0;
-		};
+		}
 
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	};
 }
 
-static void WorkHour(ImFont *font_cron, int &hours, int &minutes, int &seconds, bool &start)
+static void WorkHour(ImFont *font_inter_semibold, int &hours, int &minutes, int &seconds, bool &start)
 {
 	ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
 	ImGui::SetNextWindowPos(ImVec2(0, 0));
 
-	ImGui::Begin("Stopwatch", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+	ImGuiWindowFlags window_flags = 0;
+	window_flags |= ImGuiWindowFlags_NoTitleBar;
+	window_flags |= ImGuiWindowFlags_NoMove;
+	window_flags |= ImGuiWindowFlags_NoResize;
+	window_flags |= ImGuiWindowFlags_NoCollapse;
 
-	ImGui::PushFont(font_cron);
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.27f, 0.06f, 0.36f, 1.0f));
+	ImGui::Begin("Stopwatch", nullptr, window_flags);
+
+	ImGui::Dummy(ImVec2(0.0f, 15.0f));
+
+	ImGui::SetCursorPosX((ImGui::GetWindowSize().x - ImGui::CalcTextSize("WorkHour StopWatch").x) * 0.5f);
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+	ImGui::Text("WorkHour StopWatch");
+	ImGui::PopStyleColor();
+
 	std::string time = getTime(hours, minutes, seconds);
 
+	ImGui::Dummy(ImVec2(0.0f, 1.0f));
+
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+	ImGui::PushFont(font_inter_semibold);
+
+	ImGui::SetCursorPosX((ImGui::GetWindowSize().x - ImGui::CalcTextSize("00 : 00 : 00").x) * 0.5f);
 	ImGui::TextWrapped("%s", time.c_str());
 	ImGui::PopFont();
+	ImGui::PopStyleColor();
+
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+	ImGui::SetCursorPosX(83.0f);
+	ImGui::SetCursorPosY(130.0f);
+	ImGui::Text("Hours");
+	ImGui::SameLine();
+	ImGui::SetCursorPosX(223.0f);
+	ImGui::Text("Minutes");
+	ImGui::SameLine();
+	ImGui::SetCursorPosX(373.0f);
+	ImGui::Text("Seconds");
+	ImGui::PopStyleColor();
+
+	ImGui::Dummy(ImVec2(0.0f, 20.0f));
 
 	std::string buttonText = start ? "Pause" : "Start";
 
-	if (ImGui::Button(buttonText.c_str()))
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.90f, 0.49f, 0.85f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.87f, 0.56f, 0.83f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.87f, 0.56f, 0.83f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+	ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 40.0f);
+
+	ImGui::SetCursorPosX(130.0f);
+	if (ImGui::Button(buttonText.c_str(), ImVec2(100.0f, 24.0f)))
 	{
 		start = !start;
-		Thread = std::thread(updateTime, std::ref(hours), std::ref(minutes), std::ref(seconds), std::ref(start));
+		Thread = std::thread(updateTime, std::ref(hours),
+							 std::ref(minutes), std::ref(seconds), std::ref(start));
 		Thread.detach();
 	}
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleVar();
 
 	ImGui::SameLine();
 
-	if (ImGui::Button("Reset"))
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.34f, 0.26f, 0.38f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.41f, 0.34f, 0.45f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.41f, 0.34f, 0.45f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+	ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 40.0f);
+
+	ImGui::SetCursorPosX(248.0f);
+	if (ImGui::Button("Reset", ImVec2(100.0f, 24.0f)))
 	{
 
 		hours = 0;
@@ -74,7 +134,14 @@ static void WorkHour(ImFont *font_cron, int &hours, int &minutes, int &seconds, 
 		seconds = 0;
 	}
 
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleColor();
+	ImGui::PopStyleVar();
+
 	ImGui::End();
+	ImGui::PopStyleColor();
 }
 
 int main()
@@ -91,7 +158,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	// Create window with graphics context
-	GLFWwindow *window = glfwCreateWindow(220, 120, "WorkHour", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(452, 213, "WorkHour", nullptr, nullptr);
 	glfwSetWindowAttrib(window, GLFW_RESIZABLE, GLFW_FALSE);
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1); // Enable vsync
@@ -105,11 +172,11 @@ int main()
 
 	// load font
 	ImGuiIO &io = ImGui::GetIO();
-	io.Fonts->AddFontFromFileTTF("fonts/Roboto-Medium.ttf", 18.0f);
-	ImFont *font_cron = io.Fonts->AddFontFromFileTTF("fonts/Roboto-Medium.ttf", 47.0f);
+	io.Fonts->AddFontFromFileTTF("fonts/Inter-Regular.ttf", 13.5f);
+	ImFont *font_inter_semibold = io.Fonts->AddFontFromFileTTF("fonts/Inter-SemiBold.ttf", 90.0f);
 
 	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
+	ImGui::StyleColorsLight();
 
 	// Setup Platform/Renderer bindings
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -136,7 +203,7 @@ int main()
 		ImGui::NewFrame();
 
 		// Run our Dear ImGui application
-		WorkHour(font_cron, hours, minutes, seconds, start);
+		WorkHour(font_inter_semibold, hours, minutes, seconds, start);
 
 		// Rendering
 		ImGui::Render();
